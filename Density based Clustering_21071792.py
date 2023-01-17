2dacfac3d31d39832b8ede2e748699213c889ab7
anjithajoy18 <117921374+anjithajoy18@users.noreply.github.com>
2023-01-17 00:59:06 +0000

    Add files via upload

--- Density based Clustering_21071792.py
+++ Density based Clustering_21071792.py
@@ -20,34 +20,45 @@ def read_csv(file_name):
     file = pd.read_csv(file_name)
     dataFr = pd.DataFrame(file)
     print(dataFr.isnull().any().any())
-    sort_data = dataFr.iloc[:,[1, 22, 54, 88]]
-    return dataFr, sort_data
+    sort_data = dataFr.iloc[0:200,[1, 22, 34, 45]]
+    return sort_data
 
 
 def cluster_plot():
     
     #Getting the data loaded from csv file
-    data, plot_data = read_csv("Sales_Transactions_Dataset_Weekly.csv")
+    plot_data = read_csv("Sales_Transactions_Dataset_Weekly.csv")
     
     #standardizing the data
     plot_data = StandardScaler().fit_transform(plot_data)
+    #plot_data = pd.DataFrame(plot_data)
     
     #Normalizing the data
     plot_data = normalize(plot_data)
-    #plot_data = pd.DataFrame(plot_data)
-    print(plot_data)
+    plot_data = pd.DataFrame(plot_data)
     
+    x = plot_data[0]
+    y = plot_data[2]
+    
+
     #KNN method
-    nearest_neigh = NearestNeighbors(n_neighbors = 3).fit(plot_data)
-    neighbor_dist = nearest_neigh.kneighbors(plot_data)
+    nearest_neigh = NearestNeighbors(n_neighbors = 3).fit(plot_data) 
+    neighbor_dist, neighbor_index = nearest_neigh.kneighbors(plot_data)
     neighbor_dist = np.sort(neighbor_dist, axis = 0)
-    neighbor_dist = neighbor_dist[:, 4]
+    neighbor_dist = neighbor_dist[:, 3]
+    print(neighbor_dist)
     plt.figure(figsize = (5,3))
     plt.plot(neighbor_dist)
     plt.savefig("line plot.png")
     
-    Minpoints = 
-
+    
+    #Cluster plotting
+    minPoints = 2*(len(plot_data.axes[1]))
+    dbscan = DBSCAN(eps = 0.15, min_samples = minPoints).fit(plot_data)
+    set(dbscan.labels_)
+    
+    plt.figure()
+    plt.scatter(plot_data[:, 0],plot_data[:, 1])
     return
 
 
